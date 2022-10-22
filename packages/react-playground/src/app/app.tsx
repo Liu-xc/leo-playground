e9cd89007ed2eaf056c714b2a2691e2ddfd2b44e
Liu-xc <1392166232@qq.com>
2022-10-23 01:36:45 +0800

    feat: init dynamic form

--- packages/react-playground/src/app/app.tsx
+++ packages/react-playground/src/app/app.tsx
@@ -1,52 +1,23 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.less';
-import NxWelcome from './nx-welcome';
-
-import { Route, Routes, Link } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes, NavLink, BrowserRouter } from 'react-router-dom';
+import DynamicFormPlayground from '../pages/dynamic-form';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="react-playground" />
-      <div />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
+    <BrowserRouter>
+      <ul>
+        <li>
+          <NavLink to="/dynamic-form">/dynamic-form</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">/</NavLink>
+        </li>
+      </ul>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
+        <Route path="/" element={<div>hello react !</div>} />
+        <Route path="/dynamic-form" element={<DynamicFormPlayground />} />
       </Routes>
-      {/* END: routes */}
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ packages/react-playground/src/pages/dynamic-form/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createForm } from '@formily/core';
+import { FormProvider, Field, FormConsumer } from '@formily/react';
+import {
+  FormButtonGroup,
+  FormItem,
+  FormLayout,
+  Input,
+  Submit,
+} from '@formily/antd';
+
+const DynamicFormPlayground = () => {
+  const form = createForm();
+  return (
+    <FormProvider form={form}>
+      <FormLayout layout={'vertical'}>
+        <Field
+          name="input"
+          title="input box"
+          required
+          initialValue="hello"
+          decorator={[FormItem]}
+          component={[Input, { value: 'world' }]}
+        />
+      </FormLayout>
+      <FormConsumer>{() => <div>{form.values.input}</div>}</FormConsumer>
+      <FormButtonGroup>
+        <Submit onSubmit={() => console.log(form.values)}>submit</Submit>
+      </FormButtonGroup>
+    </FormProvider>
+  );
+};
+
+export default DynamicFormPlayground;
