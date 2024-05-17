4d05e0fea9f8cd48522be94d96df804bc87d04d6
Liu-xc <1392166232@qq.com>
2024-05-18 01:52:44 +0800

    feat: import coze bot

--- packages/nodejs/src/server/index.js
+++ packages/nodejs/src/server/index.js
@@ -1,11 +1,14 @@
 "use strict";
 exports.__esModule = true;
 var http = require("http");
+var fs = require("fs");
+const path = require("path");
 var server = http.createServer(function (req, res) {
     var urlItem = req.url;
     console.log(req.url, req.headers.host);
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    res.end('<h1>杨璐铭是猪！</h1>');
+    const content = fs.readFileSync(path.resolve(__dirname, '../template/index.html'), 'utf-8')
+    res.end(content);
 });
 server.listen(8080, function () {
     console.log('listening 8080');
