96c22ec43e20adc3de1db59430acda4a5d2f478f
Liu-xc <1392166232@qq.com>
2024-05-18 01:30:29 +0800

    fix: set content type

--- packages/nodejs/src/server/index.js
+++ packages/nodejs/src/server/index.js
@@ -4,7 +4,7 @@ var http = require("http");
 var server = http.createServer(function (req, res) {
     var urlItem = req.url;
     console.log(req.url, req.headers.host);
-    res.setHeader('Content-Type', 'application/xhtml+xml; charset=utf-8')
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
     res.end('<h1>杨璐铭是猪！</h1>');
 });
 server.listen(8080, function () {
