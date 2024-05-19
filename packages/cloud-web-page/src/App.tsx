96ff36e7769189c099a0326941281026ccccb37f
Liu-xc <1392166232@qq.com>
2024-05-19 18:08:45 +0800

    fix: title and icon

--- packages/cloud-web-page/src/App.tsx
+++ packages/cloud-web-page/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import './App.css';
-import Uehara from './assets/uehara.jpg';
+import Uehara from './assets/uehara.jpg?url';
 
 function App() {
   useEffect(() => {
@@ -11,7 +11,7 @@ function App() {
     componentProps: {
         title: 'Coze',
       layout: 'mobile',
-        icon: Uehara
+        icon: process.env.NODE_ENV === 'development' ? `http://localhost:5173${Uehara}` : `http://47.95.204.202:8080${Uehara}`
         },
       });
   }, []);
