31c8fc2eead7bb30bece881ff3dba3b54dba494b
Liu-xc <1392166232@qq.com>
2024-05-19 18:15:26 +0800

    feat: use background img to show uehara

--- packages/cloud-web-page/src/App.tsx
+++ packages/cloud-web-page/src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react'
 import './App.css';
-import Uehara from './assets/uehara.jpg?url';
 
 function App() {
   useEffect(() => {
@@ -11,7 +10,6 @@ function App() {
     componentProps: {
         title: 'Coze',
       layout: 'mobile',
-        icon: process.env.NODE_ENV === 'development' ? `http://localhost:5173${Uehara}` : `http://47.95.204.202:8080${Uehara}`
         },
       });
   }, []);
